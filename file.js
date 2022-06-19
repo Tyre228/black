470c9017a465554c640b1f40026d1e4266ce3755
Tyre228 <107572662+Tyre228@users.noreply.github.com>
2022-06-19 21:28:46 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,1 @@
-var overimage = document.querySelector(".overimage");
-function imageChanger() {
-    setTimeout(function classChanger() {overimage.className = "overimage overimage_invisited";}, 3000)
-}
-overimage.addEventListener("load", imageChanger())
+
