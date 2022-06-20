6734815538627a520b4ea771baed0d456d980c07
Tyre228 <107572662+Tyre228@users.noreply.github.com>
2022-06-20 16:28:18 +0300

    Add files via upload

--- file.js
+++ file.js
@@ -1,1 +1,18 @@
+let overimage = document.querySelector('.overimage');
+let footer = document.querySelector("footer");
+var width;
+function imageChanger() {
+    setTimeout(classChanger, 1500)
+}
+function classChanger() {
+    overimage.className = "overimage overimage_invisited";
+}
+overimage.addEventListener("load", imageChanger());
 
+
+setInterval(windowWidth, 500)
+function windowWidth() {
+    width = window.innerWidth;
+    console.log(width)
+    footer.innerHTML = `<footer>${width}</footer>`
+}
